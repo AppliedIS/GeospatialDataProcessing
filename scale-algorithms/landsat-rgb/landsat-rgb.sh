bd3046856ff639c5fabc43e9c4a4203dfdd6e624
ericsvendsen <esvendsen@gmail.com>
2016-02-22 16:32:08 -0500

    adding path info

--- scale-algorithms/landsat-rgb/landsat-rgb.sh
+++ scale-algorithms/landsat-rgb/landsat-rgb.sh
@@ -40,33 +40,27 @@ green_filename="${green_file%.*}"
 blue_file=$(basename "${blue_inputfile}")
 blue_filename="${blue_file%.*}"
 
-echo "${red_filename}"
-echo "${green_filename}"
-echo "${blue_filename}"
-
 # Extract base filename without band or extension
 base_filename="${red_file%_*}"
 
-echo "${base_filename}"
-
 # Reproject images
-gdalwarp -t_srs EPSG:3857 "${blue_inputfile}" "${blue_filename}"_PROJECTED.TIF
-gdalwarp -t_srs EPSG:3857 "${green_inputfile}" "${green_filename}"_PROJECTED.TIF
-gdalwarp -t_srs EPSG:3857 "${red_inputfile}" "${red_filename}"_PROJECTED.TIF
+gdalwarp -t_srs EPSG:3857 "${blue_inputfile}" /tmp/data/"${blue_filename}"_PROJECTED.TIF
+gdalwarp -t_srs EPSG:3857 "${green_inputfile}" /tmp/data/"${green_filename}"_PROJECTED.TIF
+gdalwarp -t_srs EPSG:3857 "${red_inputfile}" /tmp/data/"${red_filename}"_PROJECTED.TIF
 # Combine bands into one RGB image
-convert -combine "${red_filename}"_PROJECTED.TIF "${green_filename}"_PROJECTED.TIF "${blue_filename}"_PROJECTED.TIF "${base_filename}"_RGB.TIF
+convert -combine /tmp/data/"${red_filename}"_PROJECTED.TIF /tmp/data/"${green_filename}"_PROJECTED.TIF /tmp/data/"${blue_filename}"_PROJECTED.TIF /tmp/data/"${base_filename}"_RGB.TIF
 # Adjust image color
-convert -channel B -gamma 0.975 -channel G -gamma 0.99 -channel RGB -sigmoidal-contrast 50x13% "${base_filename}"_RGB.TIF "${base_filename}"_RGB_CORRECTED.TIF
+convert -channel B -gamma 0.975 -channel G -gamma 0.99 -channel RGB -sigmoidal-contrast 50x13% /tmp/data/"${base_filename}"_RGB.TIF /tmp/data/"${base_filename}"_RGB_CORRECTED.TIF
 # Convert to 8 bit image
-convert -depth 8 "${base_filename}"_RGB_CORRECTED.TIF "${base_filename}"_RGB_CORRECTED_8bit.TIF
+convert -depth 8 /tmp/data/"${base_filename}"_RGB_CORRECTED.TIF /tmp/data/"${base_filename}"_RGB_CORRECTED_8bit.TIF
 # Retrieve geo data from base image and reapply it to the corrected 8-bit RGB image
-listgeo -tfw "${red_filename}"_PROJECTED.TIF
-mv "${red_filename}"_PROJECTED.tfw "${base_filename}"_RGB_CORRECTED_8bit.tfw
-gdal_edit.py -a_srs EPSG:3857 "${base_filename}"_RGB_CORRECTED_8bit.TIF
+listgeo -tfw /tmp/data/"${red_filename}"_PROJECTED.TIF
+mv /tmp/data/"${red_filename}"_PROJECTED.tfw /tmp/data/"${base_filename}"_RGB_CORRECTED_8bit.tfw
+gdal_edit.py -a_srs EPSG:3857 /tmp/data/"${base_filename}"_RGB_CORRECTED_8bit.TIF
 # Toss all input files
 ls | grep -v _8bit.TIF | xargs rm
 
-mv "${base_filename}"_RGB_CORRECTED_8bit.TIF "${@: -1}"/"${base_filename}"_RGB_CORRECTED_8bit.TIF
+mv /tmp/data/"${base_filename}"_RGB_CORRECTED_8bit.TIF "${@: -1}"/"${base_filename}"_RGB_CORRECTED_8bit.TIF
 
 cat > "${@: -1}"/results_manifest.json << EOF
 { "version": "1.1",
