60ded0fbfc3e67cc350e55d1f4ae75ea51bcd6f9
Chris Dickman <chris.dickman@appliedis.com>
2016-03-09 15:41:48 -0500

    fixed curl command in jobs that push to geoserver

--- scale-algorithms/publish-geoserver-raster/publish-geoserver-raster.sh
+++ scale-algorithms/publish-geoserver-raster/publish-geoserver-raster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Pushing to geoserver: " $1
-curl —form "file=@$1" http://10.4.4.100:9000/importraster/
+curl —=form "file=@$1" http://10.4.4.100:9000/importraster/
--- scale-algorithms/publish-hotspots/publish-hotspots.sh
+++ scale-algorithms/publish-hotspots/publish-hotspots.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Pushing to geoserver: " $1
-curl —form "file=@$1" --form "store=landsat-hotspots" http://10.4.4.100:3000/mosaic/update/
+curl --form "file=@$1" --form "store=landsat-hotspots" http://10.4.4.100:3000/mosaic/update
