36f71b660bde37aaffcb8c93dbabfd1392855aed
Chris Dickman <chris.dickman@appliedis.com>
2016-03-09 15:48:20 -0500

    fixed store name in publish-hotspots job

--- scale-algorithms/publish-hotspots/publish-hotspots.sh
+++ scale-algorithms/publish-hotspots/publish-hotspots.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Pushing to geoserver: " $1
-curl --form "file=@$1" --form "store=landsat-hotspots" http://10.4.4.100:3000/mosaic/update
+curl --form "file=@$1" --form "store=hotspots" http://10.4.4.100:3000/mosaic/update
